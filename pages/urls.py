9ee386008b0287065c64e3f907c574ff4a31fda6
jakubwi <jakubwi@github.com>
2018-09-19 13:44:33 +0200

    adding card directly to deck done

--- pages/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.AboutPageView.as_view(), name='about'),
-]
--- pages/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-class AboutPageView(TemplateView):
-    template_name = 'about.html'
